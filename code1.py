4383d46fe5a4473573f5f7450745a7c31b782347
Ritwik Ghosh <gritwik5843@gmail.com>
2022-03-10 22:05:07 +0530

    Merge branch 'main' of https://github.com/CoderRitwik/1.-python-project

--- code1.py
+++ code1.py
@@ -1,2 +1,14 @@
-print("hellow NEWGIT")
-print("ok")
+#get details of mobile number 😉
+import phonenumbers
+from test import number
+""""test is a file name where i store my phone number with 
+country code in number variable 😊😊😊""" 
+
+from phonenumbers import geocoder
+ch_nmber=phonenumbers.parse(number, "CH") #here we store the country name
+print(geocoder.description_for_number(ch_nmber, "en"))
+
+from phonenumbers import carrier #carrier use for service provider name like jio ,vi
+service_name=phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_name, "en"))
+#i give my jio number but it does not show,i think they did not develop the option for jio😟
